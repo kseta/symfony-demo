e022128b9e85255ff59cb015c471ba277eba0df0
kseta <keigo.seta@gmail.com>
2015-02-15 20:25:22 +0900

    [app] Fix deploy configurations for staging

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -1,19 +1,38 @@
-set :application, "set your application name here"
-set :domain,      "#{application}.com"
-set :deploy_to,   "/var/www/#{domain}"
-set :app_path,    "app"
-
-set :repository,  "#{domain}:/var/repos/#{application}.git"
-set :scm,         :git
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `subversion`, `mercurial`, `perforce`, or `none`
-
+# Application
+set :application,   "SymfonyDemoAppForCI"
+set :domain,        "symfony-demo-staging"
+set :app_path,      "app"
 set :model_manager, "doctrine"
-# Or: `propel`
 
-role :web,        domain                         # Your HTTP server, Apache/etc
-role :app,        domain, :primary => true       # This may be the same as your `Web` server
+# Application Reposiroty
+set :repository, "git@github.com:kseta/SymfonyDemoAppForCI.git"
+set :scm,        :git
 
-set  :keep_releases,  3
+# Deploy role
+role :web, domain
+role :app, domain, :primary => true
+
+# Deploy User
+set :user, "vagrant"
+
+# Deploy Server
+set :deploy_to,  "/var/www/symfony-demo.com"
+set :deploy_via, :copy
+set :use_sudo,   false
+
+# Assetic
+set :dump_assetic_assets, true
+
+# Shared
+set :shared_files,    ["composer.phar", "app/config/parameters.yml"]
+set :shared_children, [app_path + "/logs", web_path + "/uploads", "vendor"]
+
+# Dependencies
+set :use_composer,   true
+set :update_vendors, false
+
+# Ignore
+set :copy_exclude, [".git/*", ".gitignore"]
 
 # Be more verbose by uncommenting the following line
 # logger.level = Logger::MAX_LEVEL
